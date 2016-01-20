514b83a19cd4de77771ea65b9dcf77b03ba18266
ytakani <y-takanishi@newral.co.jp>
2016-01-20 11:11:03 +0900

    Form1.cs top にコメント追加

--- Test1/Form1.cs
+++ Test1/Form1.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿//-----------------------------------------------
+//  GitHub, SourceTree 実験用 Project
+//
+//  ソース自体に意味はありません。
+//  commit, push, pull 確認のための Project
+//-----------------------------------------------
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
