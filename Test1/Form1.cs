4d6a251c95c8ecf3eab6af50d50d8737701bca7d
ytakani <y-takanishi@newral.co.jp>
2016-01-20 11:01:49 +0900

    Form1.cs にコメント追加、Debugコンパイルした。

--- Test1/Form1.cs
+++ Test1/Form1.cs
@@ -10,7 +10,9 @@ using System.Windows.Forms;
 
 namespace Test1
 {
-	//-----------------------------------
+    //-----------------------------------
+    // Test Form1
+    //-----------------------------------
     public partial class Form1 : Form
     {
         public Form1()
